d88697e9dd3f8edc04a4674da86db0789a2feb89
abeerqqq <abeer-198@hotmail.com>
2024-01-23 18:16:04 +0300

    DTO

---
+++ Magic_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,28 @@
+﻿using Magic_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magic_VillaAPI.Controllers
+{
+    /* [ApiController]
+     * attribute is used to decorate a controller class, indicating that it is an API controller.
+     * This attribute provides certain behaviors and conventions specific to API controllers*/
+    [ApiController]
+    [Route("api/VillaAPI")]
+    /*Our api controller has to extends from ControllerBase we prev used : Controller for hnadling Razor views in MVC
+     we need to use icrosoft.AspNetCore.Mvc;
+    ControllerBase documentaion
+     */
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            IEnumerable<VillaDTO> VillasList = new List<VillaDTO>() { 
+            
+            new VillaDTO{Id=1, Name="Pool View"},
+            new VillaDTO{Id=2,Name="Beach View"}
+            };
+            return VillasList;
+        }
+    }
+}
---
+++ Magic_VillaAPI/Models/Villa.cs
@@ -0,0 +1,10 @@
+﻿namespace Magic_VillaAPI.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // we do not want the creation time to appear in the API
+        public DateTime CreateAt { get; set; }
+    }
+}
